7776a40d4e4a7ecdb76c1fb09e5b8400280b90d4
kimseyun314 <kimseyun314@naver.com>
2021-05-28 18:53:13 +0900

    delete space of title

--- recipe_crawl.py
+++ recipe_crawl.py
@@ -6,6 +6,9 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask
 
+def hfilter(s):
+	return re.sub(u'[^ \.\,\?\!u3130-\u318f\uac00-\ud7a3]+','',s)
+
 situation = {'선택안함':'0','일상':'12', '초스피드':'18', '손님접대':'13'}
 if __name__ == '__main__':
 	sit = input()
@@ -26,9 +29,9 @@ if __name__ == '__main__':
 	new_url = url+"소고기"+"+"+"계란"+url2+url_cat2+url_lastcate
 	res = requests.get(new_url)
 	html = BeautifulSoup(res.content, "html.parser")
-	tags = html.select('li[class="common_sp_list_li"]')
-
+	tags_title = html.select('div[class="common_sp_caption_tit line2"]')
+	#tags_link = html.select('
 	for i in range(0,10):
-		html_content = tags[i].text
+		html_content = hfilter(tags_title[i].text)
 		print(html_content)		
 #print(new_url)
