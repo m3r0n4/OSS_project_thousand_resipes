028336d80a84dedb853118243e477a99615522f5
kimseyun314 <kimseyun314@naver.com>
2021-05-28 18:44:42 +0900

    get text update

--- recipe_crawl.py
+++ recipe_crawl.py
@@ -24,5 +24,11 @@ if __name__ == '__main__':
 	url_cat2 = '&cat3=&cat4=&fct=&order=accuracy&lastcate='+lastcate
 	url_lastcate = '&dsearch=&copyshot=&scrap=&degree=&portion=&time=&niresource='
 	new_url = url+"소고기"+"+"+"계란"+url2+url_cat2+url_lastcate
-	print(new_url)
+	res = requests.get(new_url)
+	html = BeautifulSoup(res.content, "html.parser")
+	tags = html.select('li[class="common_sp_list_li"]')
 
+	for i in range(0,10):
+		html_content = tags[i].text
+		print(html_content)		
+#print(new_url)
