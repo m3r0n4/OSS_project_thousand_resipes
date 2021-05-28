63e0438cac2b8be6b5683cbce66a575083372b98
kimseyun314 <kimseyun314@naver.com>
2021-05-28 19:46:50 +0900

    crawl title and href-link

--- recipe_crawl.py
+++ recipe_crawl.py
@@ -9,7 +9,7 @@ from flask import Flask
 def hfilter(s):
 	return re.sub(u'[^ \.\,\?\!u3130-\u318f\uac00-\ud7a3]+','',s)
 
-situation = {'선택안함':'0','일상':'12', '초스피드':'18', '손님접대':'13'}
+situation = {'선택안함':'0','일상':'12', '초스피드':'18', '손님접대':'13', '간식':'17', '다이어트':'21', '야식':'45'}
 if __name__ == '__main__':
 	sit = input()
 	lastcate = ""
@@ -30,8 +30,19 @@ if __name__ == '__main__':
 	res = requests.get(new_url)
 	html = BeautifulSoup(res.content, "html.parser")
 	tags_title = html.select('div[class="common_sp_caption_tit line2"]')
-	#tags_link = html.select('
-	for i in range(0,10):
-		html_content = hfilter(tags_title[i].text)
-		print(html_content)		
+	tags_link = html.select('a[class="common_sp_link"]')
+#	for i in range(0,10):
+	for i in range(0, len(tags_title)):
+		html_content_title = hfilter(tags_title[i].text)
+		print(html_content_title)		
+#	for i in range(0,10):
+#		html_content_link = tags_link[i]
+#		print(html_content_link)
 #print(new_url)
+	count = 0
+	for link in tags_link:
+		if count == 10:
+			break
+		link_url = "https://www.10000recipe.com"+link["href"]
+		print(link_url)
+		count = count+1
