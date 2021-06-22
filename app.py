da4e711981591040104243d7a9fb8c4c7c32584a
m3r0n4 <mingi0095@naver.com>
2021-06-22 16:59:24 +0900

    commit

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
+#!/usr/bin/python
+#-*-coding:utf-8-*-
+
 from flask import *
 
-app = FLASK(__name__)
+app = Flask(__name__)
 
-@app.route('/home', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
 def home():
+    error = None
     return render_template('home.html')
+
 @app.route('/step1', methods=["GET", "POST"])
 def step1():
+    return render_template('step1.html')
 
 @app.route('/step2', methods=["GET", "POST"])
 def step2():
+    return render_template('step2.html')
 
 @app.route('/step3', methods=["GET", "POST"])
+def step3():
+    return render_template('step3.html')
 
 @app.route('/step4', methods=["GET", "POST"])
-
+def step4():
+    return render_template('step4.html')
 if __name__ == '__main__':
     app.run()
