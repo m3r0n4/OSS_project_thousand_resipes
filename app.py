45336b44b31fd1ecc83e5316666e7b43f4db6881
m3r0n4 <mingi0095@naver.com>
2021-06-22 16:33:11 +0900

    commit

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import *
 app = FLASK(__name__)
 
 @app.route('/home', methods=["GET", "POST"])
-
+def home():
+    
 @app.route('/step1', methods=["GET", "POST"])
+def step1():
 
 @app.route('/step2', methods=["GET", "POST"])
+def step2():
 
 @app.route('/step3', methods=["GET", "POST"])
 
