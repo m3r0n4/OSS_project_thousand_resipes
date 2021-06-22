404bd9dd062ae87ef919017c135bcb4d24cddd71
m3r0n4 <mingi0095@naver.com>
2021-06-23 04:03:28 +0900

    commit

--- app.py
+++ app.py
@@ -46,7 +46,29 @@ def step3():
 
 @app.route('/result', methods=["GET", "POST"])
 def result():
-    return render_template('result.html')
+    error = None
+    if request.method == 'POST':
+        gri1 = request.form['gri1']
+        gri2 = request.form['gri2']
+        gri3 = request.form['gri3']
+        cate = request.form['cate']
+        want = request.form['want']
+        result1 = []
+        result2 = []
+        result3 = []
+        result1, result2, result3 = crawling(gri1, gri2, gri3, cate)
+        return render_template('result.html', name1 = result1[0], name2 = result2[0], name3 = result3[0], link1 = result1[1], link2 = result2[1], link3 = result3[1], img1 = result1[2], img2 = result2[2], img3 = result3[2], gri1 = result1[3], gri2 = result2[3], gri3 = result3[3])
+    else:
+        gri1 = request.args.get['gri1']
+        gri2 = request.args.get['gri2']
+        gri3 = request.args.get['gri3']
+        cate = request.args.get['cate']
+        want = request.args.get['want']
+        result1 = []
+        result2 = []
+        result3 = []
+        result1, result2, result3 = crawling(gri1, gri2, gri3, cate)
+        return render_template('result.html', name1 = result1[0], name2 = result2[0], name3 = result3[0], link1 = result1[1], link2 = result2[1], link3 = result3[1], img1 = result1[2], img2 = result2[2], img3 = result3[2], gri1 = result1[3], gri2 = result2[3], gri3 = result3[3])
     
 if __name__ == '__main__':
     app.run()
